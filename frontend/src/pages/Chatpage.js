1c2c48b12998ab92650e140b97c5bea2cc646bb3
christian <habinezac15@gmail.com>
2022-10-21 20:37:38 +0200

    chat page UI added

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,8 +1,28 @@
 
+import { ChatState } from "../context/ChatProvider";
+import { Box } from '@chakra-ui/layout'
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const Chatpage = () => {
+
+    const { user } = ChatState()
+
     return (
-        <div></div>
+        <div style={{ width: '100%' }}>
+            {user && <SideDrawer />}
+            <Box
+                display='flex'
+                justifyContent="space-between"
+                w="100%"
+                h="91.5vh"
+                p="10px"
+            >
+                {user && <MyChats />}
+                {user && <ChatBox />}
+            </Box>
+        </div>
     );
 }
 
